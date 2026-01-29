36ce6a6cb4e9f2895dbe04b1e97236b2365f7b56
lgpalominoa <luisguil@yahoo.com>
2026-01-29 17:51:55 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 from sales.ui import render_sales
 
+# ==============================
+# Inicialización del Session State
+# ==============================
+if "auth" not in st.session_state:
+    st.session_state.auth = False
+
+if "user" not in st.session_state:
+    st.session_state.user = None
+
+if "rol" not in st.session_state:
+    st.session_state.rol = None
+
+if "auth_step" not in st.session_state:
+    st.session_state.auth_step = "login"
+
 st.set_page_config(page_title="SAC Enterprise Pro", layout="wide")
 
 if "auth" not in st.session_state:
