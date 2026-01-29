d974f741898de74b82858f4b2322f25eb2d517a0
lgpalominoa <luisguil@yahoo.com>
2026-01-29 18:18:29 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
 import streamlit as st
 from sales.ui import render_sales
 
+import streamlit as st
+
+st.title("🔥 Streamlit funcionando correctamente")
+st.write("Si ves esto, el problema NO es Streamlit")
+
+
 # ==============================
 # Inicialización del Session State
 # ==============================
-if "auth" not in st.session_state:
-    st.session_state.auth = False
+# if "auth" not in st.session_state:
+    # st.session_state.auth = False
 
-if "user" not in st.session_state:
-    st.session_state.user = None
+# if "user" not in st.session_state:
+    # st.session_state.user = None
 
-if "rol" not in st.session_state:
-    st.session_state.rol = None
+# if "rol" not in st.session_state:
+    # st.session_state.rol = None
 
-if "auth_step" not in st.session_state:
-    st.session_state.auth_step = "login"
+# if "auth_step" not in st.session_state:
+    # st.session_state.auth_step = "login"
 
-st.set_page_config(page_title="SAC Enterprise Pro", layout="wide")
+# st.set_page_config(page_title="SAC Enterprise Pro", layout="wide")
 
-if "auth" not in st.session_state:
-    st.session_state.auth = True
-    st.session_state.user = "admin"
+# if "auth" not in st.session_state:
+    # st.session_state.auth = True
+    # st.session_state.user = "admin"
 
-if st.session_state.auth:
-    render_sales(st.session_state.user)
+# if st.session_state.auth:
+    # render_sales(st.session_state.user)
